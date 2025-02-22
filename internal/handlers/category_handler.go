ea13b173db8dfcd56101c8155e21a5f86c09b181
pinklaosa <rps53009@hotmail.com>
2025-02-22 16:50:17 +0700

    Refactor: using base in handler

--- internal/handlers/category_handler.go
+++ internal/handlers/category_handler.go
@@ -9,76 +9,74 @@ import (
 )
 
 type CategoryHandler struct {
+	base BaseHandler
 	services services.CategoryService
 }
 
 func NewCategoryHandler(services services.CategoryService) *CategoryHandler {
-	return &CategoryHandler{services: services}
+	return &CategoryHandler{ 
+		base: *NewBaseHandler(),	
+		services: services}
 }
 
 func (h *CategoryHandler) GetAllCategory(c echo.Context) error {
-	base := NewBaseHandler()
 	result, err := h.services.GetAllCategory()
 	if err != nil {
-		return base.JSONBadRequest(c, err)
+		return h.base.JSONBadRequest(c, err)
 	}
-	return base.JSONSuccess(c, result)
+	return h.base.JSONSuccess(c, result)
 }
 
 func (h *CategoryHandler) GetCategoryById(c echo.Context) error {
-	base := NewBaseHandler()
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return base.JSONBadRequest(c, err)
+		return h.base.JSONBadRequest(c, err)
 	}
 	category, err := h.services.GetCategoryById(id)
 	if err != nil {
-		return base.JSONNotFound(c, err)
+		return h.base.JSONNotFound(c, err)
 	}
-	return base.JSONSuccess(c, category)
+	return h.base.JSONSuccess(c, category)
 }
 
 func (h *CategoryHandler) CreateCategory(c echo.Context) error {
-	base := NewBaseHandler()
 	category := new(models.Category)
 	if err := c.Bind(&category); err != nil {
-		return base.JSONBadRequest(c, err)
+		return h.base.JSONBadRequest(c, err)
 	}
 
 	if err := h.services.CreateCategory(category); err != nil {
-		return base.JSONInternalServer(c, err)
+		return h.base.JSONInternalServer(c, err)
 	}
 
-	return base.JSONCreated(c, category)
+	return h.base.JSONCreated(c, category)
 }
 
 func (h *CategoryHandler) UpdateCategory(c echo.Context) error {
-	base := NewBaseHandler()
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return base.JSONBadRequest(c, err)
+		return h.base.JSONBadRequest(c, err)
 	}
 
 	updatedCategory := new(models.Category)
 	if err := c.Bind(&updatedCategory); err != nil {
-		return base.JSONBadRequest(c, err)
+		return h.base.JSONBadRequest(c, err)
 	}
 	if err := h.services.UpdateCategory(id, updatedCategory); err != nil {
-		return base.JSONNotFound(c, err)
+		return h.base.JSONNotFound(c, err)
 	}
-	return base.JSONSuccess(c, updatedCategory)
+	return h.base.JSONSuccess(c, updatedCategory)
 }
 
 func (h *CategoryHandler) DeleteCategory(c echo.Context) error {
-	base := NewBaseHandler()
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return base.JSONBadRequest(c, err)
+		return h.base.JSONBadRequest(c, err)
 	}
 
 	if err := h.services.DeleteCategory(id); err != nil {
-		return base.JSONInternalServer(c, err)
+		return h.base.JSONInternalServer(c, err)
 	}
 
-	return base.JSONSuccess(c, "Deleted")
+	return h.base.JSONSuccess(c, "Deleted")
 }
