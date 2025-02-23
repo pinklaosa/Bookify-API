9c36a5865e1515f945a07bd5fcaf83b54e66124a
pinklaosa <rps53009@hotmail.com>
2025-02-23 18:23:10 +0700

    Fix: bug middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,17 +1,13 @@
 package middleware
 
 import (
-	"Bookify/internal/config"
-
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
 // JWT Middleware
 func JWTMiddleware(secret string) echo.MiddlewareFunc {
-	cfk := config.AppConfigInstance.Key
 	return echojwt.WithConfig(echojwt.Config{
-		SigningKey:  []byte(cfk.SecretKey),
-		TokenLookup: "header:Authorization",
+		SigningKey:  []byte(secret),
 	})
 }
